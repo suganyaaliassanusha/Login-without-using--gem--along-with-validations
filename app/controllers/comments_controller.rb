969ac8a8a365ffd6d2adb0e6b9c361bfdfd8dc6d
suganyaaliassanusha <suganya.selvarajan@in.reportbee.com>
2014-06-18 14:45:59 +0530

    second commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  
+  
+  def create
+    @user= User.find(params[:user_id])
+    @comment= @user.comments.create(comment_params)
+     redirect_to user_path(@user)
+  end
+  def destroy
+   @user= User.find(params[:user_id])
+   @comment=@user.comments.find(params[:id])
+   @comment.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+   def comment_params
+      params.require(:comment).permit(:commenter, :body)
+   end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
- has_many :comments
+ has_many :comments, dependent: :destroy
  validates :name, presence: true, length: {minimum: 5 }
 end
